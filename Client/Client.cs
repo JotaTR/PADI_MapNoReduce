0c8cae059997d76c2d8b8409768675789add316c
MugiRenas <renato_correia1@hotmail.com>
2015-05-21 10:47:32 +0100

    Changed client
    deliver tasks mostly implemented

--- Client/Client.cs
+++ Client/Client.cs
@@ -11,39 +11,51 @@ using System.Collections.Generic;
 
 namespace PADI_MapNoReduce
 {
-    class Program
+    public class Program
     {
 
-        public void Init(String EntryURL)
+        public Cliente Init(int clientID, String EntryURL)
         {
-            TcpChannel channel = new TcpChannel(Int32.Parse(EntryURL));
-            ChannelServices.RegisterChannel(channel, true);
+            Cliente c = new Cliente(clientID, EntryURL);
+
+            TcpChannel channel = new TcpChannel(10000 + clientID);
+
+            ChannelServices.RegisterChannel(channel, false);
+
+            RemotingServices.Marshal(c, "C", typeof(Cliente));
+
+            return c;
+        }
+
+        public Program()
+        {
+
         }
 
         static void Main(string[] args)
         {
-            string mapperName = args[0];
+            /*string mapperName = args[0];
             int splitNumber = Int32.Parse(args[3]);
             String clientURL = args[4];
             String file = args[5];
-            WorkerInterfaceRef mt = (WorkerInterfaceRef)Activator.GetObject(
+            /*WorkerInterfaceRef mt = (WorkerInterfaceRef)Activator.GetObject(
                 typeof(WorkerInterfaceRef),
                 "tcp://localhost:30001/W");
             try
             {
                 byte[] code = File.ReadAllBytes(args[2]);
-                Console.WriteLine(mt.SendMapperService(code, mapperName, splitNumber, clientURL, file));
+                Console.WriteLine(mt.submitJobService(splitNumber, clientURL, file));
             }
             catch (SocketException)
             {
                 System.Console.WriteLine("Could not locate server");
-            }
+            }*/
             Console.ReadLine();
         }
     }
 
 
-        class Cliente
+        class Cliente : MarshalByRefObject
         {
             private bool isWaitingResult = false;
             private string _inputFilePath;
@@ -53,14 +65,14 @@ namespace PADI_MapNoReduce
             private string _mapImplementPath;
             private IList<KeyValuePair<string, string>> _result;
             private string[] _textSplit;
-            private static TcpChannel channel;
-            String idCliente;
+            int idCliente;
+            String workerURL;
 
 
-            public Cliente(TcpChannel canal, String id)
+            public Cliente(int id, string entryURL)
             {
-                channel = canal;
                 idCliente = id;
+                workerURL = entryURL;
             }
 
             public void submit(string inputPath, int splits, string outputPath, IMapper mapper, string dllPath)
@@ -68,11 +80,11 @@ namespace PADI_MapNoReduce
                 while (isWaitingResult)
                 { }
                 isWaitingResult = true;
-                _inputFilePath = inputPath;
-                _nSplits = splits;
+                //_inputFilePath = inputPath;
+                //_nSplits = splits;
                 _outputPath = outputPath;
-                _mapImplementName = mapper;
-                _mapImplementPath = dllPath;
+                //_mapImplementName = mapper;
+                //_mapImplementPath = dllPath;
 
                 string txt = System.IO.File.ReadAllText(_inputFilePath);
 
@@ -80,19 +92,48 @@ namespace PADI_MapNoReduce
                 _textSplit = txt.Split(characterSpliters);
 
                 // Send job to workers?
-                Worker worker = (Worker)Activator.GetObject(typeof(Worker), "tcp://localhost:30001/W");
+                WorkerInterfaceRef mt = (WorkerInterfaceRef)Activator.GetObject(
+                typeof(WorkerInterfaceRef),
+                workerURL);
                 byte[] code = System.IO.File.ReadAllBytes(dllPath);
+                String resposta = "";
+                resposta = mt.submitJobService(splits, "tcp://localhost:" + (10000 + idCliente) + "/C", txt);
+                if(resposta == "ok")
+                {
+
+                }
+                else
+                {
+                    mt = (WorkerInterfaceRef)Activator.GetObject(
+                        typeof(WorkerInterfaceRef),
+                        resposta);
+                }
+              
             }
 
-            
         }
 
      //Serviços disponibilizadios Pelo no
     internal class ClientServices : MarshalByRefObject, ClientInterface
     {
 
-        public SharedClass provideTask(int taskId, String text_file)
+        private IList<KeyValuePair<string, string>> _result;
+        private string[] _textSplit;
+      
+        currentsplit
+        nsplits
+         
+
+        public SharedClass provideTask(int taskId, byte[] code, String split, String className, String text_file)
         {
+            SharedClass result;
+            result.code = code;
+            result.split = split;
+            result.className = className;
+            string txt = System.IO.File.ReadAllText(_inputFilePath);
+
+            char[] characterSpliters = { '\n', '\r' };
+            _textSplit = txt.Split(characterSpliters);
             int nLines = start - end;
             string[] result = new string[nLines];
             for (int i = 0; i < nLines; i++)
@@ -106,8 +147,21 @@ namespace PADI_MapNoReduce
         {
             _result = result;
             // Send result to output path
+            if (!System.IO.File.Exists(outputPath))
+            {
+                System.IO.Directory.CreateDirectory(outputPath);
 
-            isWaitingResult = false;
+            }
+
+            for (int i = currentsplit; i < result.Count; i++)
+            {
+                string filename = "./" + outputPath + "/" + i + ".out";
+                System.IO.File.WriteAllText(filename, result[i].Key + ": " + result[i].Value);
+                currentsplit = i;
+            }
+            if(currentsplit >= nsplits){
+                isWaitingResult = false;
+            }
         }
 
     }
